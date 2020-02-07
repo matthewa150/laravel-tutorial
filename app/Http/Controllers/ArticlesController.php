6a986122121c7dfa502a1405019450c7e0e32751
Matthew Anderson <manders@pop-os.localdomain>
2020-02-06 22:13:56 -0600

    Lesson 21

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -9,6 +9,8 @@ class ArticlesController extends Controller
 {
     public function index()
     {
+        // Render a list of a resource.
+
         $articles = Article::latest()->get();
 
         return view('articles.index', ['articles' => $articles]);
@@ -16,9 +18,36 @@ class ArticlesController extends Controller
 
     public function show($id)
     {
+        // Show a single resource.
+
         $article = Article::find($id);
 
 
         return view('articles.show', ['article' => $article]);
+    }
+
+    public function create()
+    {
+        // Shows a view to create a new resource
+    }
+
+    public function store()
+    {
+        // Persist the new resource.
+    }
+
+    public function edit()
+    {
+        // Show a view to edit an existing resource.
+    }
+
+    public function update()
+    {
+        // Persist the edited resource.
+    }
+
+    public function destroy()
+    {
+        // Delete the resource.
     }
 }
