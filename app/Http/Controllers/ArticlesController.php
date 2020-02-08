c9f4cae820af9d18a2ad90eb3584a98333d8a46d
Matthew Anderson <manders@pop-os.localdomain>
2020-02-07 22:53:31 -0600

    Lesson 24

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -51,14 +51,27 @@ class ArticlesController extends Controller
         return redirect('/articles');
     }
 
-    public function edit()
+    public function edit($id)
     {
         // Show a view to edit an existing resource.
+
+        $article = Article::find($id);
+
+        return view('articles.edit', compact('article'));
     }
 
-    public function update()
+    public function update($id)
     {
         // Persist the edited resource.
+
+        $article = Article::find($id);
+        $article->title = request('title');
+        $article->excerpt = request('excerpt');
+        $article->body = request('body');
+
+        $article->save();
+
+        return redirect('/articles/' . $article->id);
     }
 
     public function destroy()
